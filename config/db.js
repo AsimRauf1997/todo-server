cf316bf9518017592a947357a18c0043030a46aa
asimrauf-sybrid <102941062+asimrauf-sybrid@users.noreply.github.com>
2022-06-18 14:30:31 +0500

    changes done

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+console.log(process.env.DB_URL);
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.DB_URL,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true 
-        })
-        console.log(`Database Conneted ${conn.connection.host}`)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-}
-module.exports = connectDB
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database Conneted ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express =  require('express')
-const todoRoutes = require('./routes/todoRoutes')
-const app = express()
-const cors =  require('cors')
-app.use(cors(
-   { origin:'*',}
-))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
-const connectDB = require('./config/db')
-const port  = process.env.PORT || 5000
+const express = require("express");
+const todoRoutes = require("./routes/todoRoutes");
+const app = express();
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-app.use('/api/todo',todoRoutes)
-app.use('/',)
-app.listen(port,()=> console.log(`Server Running at port ${port}`))
+app.use("/api/todo", todoRoutes);
+app.use("/", (req, res) => {
+  res.send("<h1>Welcome to server</h1>");
+});
+app.listen(port, () => console.log(`Server Running at port ${port}`));
